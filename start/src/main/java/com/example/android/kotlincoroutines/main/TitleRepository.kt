5fe74a9a99247c3089162a888b18a5399330d399
oliveira-marcio <emaildomso@gmail.com>
2021-05-12 19:43:45 +0100

    feat: Add unit tests for suspend functions in repository

--- start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
+++ start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
@@ -20,6 +20,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 
 /**
  * TitleRepository provides an interface to fetch a title or request a new one be generated.
@@ -51,7 +52,9 @@ class TitleRepository(val network: MainNetwork, val titleDao: TitleDao) {
     suspend fun refreshTitle() {
         try {
             // Make network request using a blocking call
-            val result = network.fetchNextTitle()
+            val result = withTimeout(5_000) {
+                network.fetchNextTitle()
+            }
             titleDao.insertTitle(Title(result))
         } catch (cause: Throwable) {
             // If anything throws an exception, inform the caller
